c8af74b4f7d2671a69c093ea8f6680475de0d8f4
jaynopponep <34385282+jaynopponep@users.noreply.github.com>
2024-05-27 19:54:02 -0400

    removed stray {children}

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,6 @@ export default function RootLayout({
         <main className="flex-grow">{children}</main>
         <Navbar />
         <Footer/>
-        {children}
       </body>
     </html>
   );
