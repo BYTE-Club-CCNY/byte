02b8419dff9312fb8167b8db0f4db7002b017d1f
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-05-27 16:21:24 -0400

    feat:navbar done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,3 +2,31 @@
 import React from "react";
 import { FloatingNav } from "./ui/floating-navbar";
 import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+
+export default function Navbar() {
+  const navItems = [
+    {
+      name: "Logo",
+      link: "/",
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dar:text-white" />,
+    },
+    {
+      name: "Projects",
+      link: "/projects",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+    {
+      name: "About",
+      link: "/about",
+      // TODO :  Remove paranthases in case they aren't needed
+      icon: <IconUser className="h-4 w-4 text-neutral-500 dar:text-white" />,
+    },
+  ];
+  return (
+    <div className="relative w-full">
+      <FloatingNav navItems={navItems} />
+    </div>
+  );
+}
--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -71,7 +71,7 @@ export const FloatingNav = ({ navItems, className }: FloatingNavProps) => {
         ))}
         <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
           <Link href="/contact">
-            <span>Contact Me</span>
+            <span>Contact Us</span>
           </Link>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
         </button>
