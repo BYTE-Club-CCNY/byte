0ed633039f118504dfcf3cedba64a17a454f9a92
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-05-27 13:26:07 -0400

    feat:mid-update

---
+++ src/components/ui/container-scroll-animation.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useState, useRef, useEffect } from "react";
+import { useScroll, useTransform, motion, MotionValue } from "framer-motion";
+
+interface ContainerScrollProps {
+  titleComponent: string | React.ReactNode;
+  children: React.ReactNode;
+}
+
+export const ContainerScroll = ({
+  titleComponent,
+  children,
+}: ContainerScrollProps) => {
+  const containerRef = useRef<any>(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+  });
+
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => {
+      window.removeEventListener("resize", checkMobile);
+    };
+  }, []);
+
+  const scaleDimensions = () => {
+    return isMobile ? [0.7, 0.9] : [1.05, 1];
+  };
+
+  const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
+};
