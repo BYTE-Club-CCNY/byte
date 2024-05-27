0ba1ba4748774d1b8a6da0735294bcffd52dc9d4
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-05-27 16:59:36 -0400

    chore:some additional implementations

--- src/components/ui/lamp.tsx
+++ src/components/ui/lamp.tsx
@@ -15,7 +15,44 @@ export function LampDemo() {
           ease: "easeInOut",
         }}
         className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center"
-      ></motion.h1>
+      >
+        Build lamps <br /> the right way
+      </motion.h1>
     </LampContainer>
   );
 }
+
+export interface LampContainerProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const LampContainer = ({ children, className }: LampContainerProps) => {
+  return (
+    <div
+      className={cn(
+        "relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-slate-950 w-full rounded-md z-0",
+        className
+      )}
+    >
+      <div className="relative flex w-full flex-1 scale-y-125 items-center justify-center isolate z-0">
+        <motion.div
+          initial={{ opacity: 0.5, width: "15rem" }}
+          whileInView={{ opacity: 1, width: "30rem" }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          style={{
+            backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
+          }}
+          className="absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent text-white [--conic-position:from_70def_at_center_top]"
+        >
+          <div className="absolute w-[100%] left-0 bg-slate-950 h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top, white, transparent)]" />
+          <div className="absolute w-40 h-[100%] left-0 bg-slate-950 bottom-0 z-20 [mask-image:linear-gradient(to_right, white, transparent)]" />
+        </motion.div>
+      </div>
+    </div>
+  );
+};
