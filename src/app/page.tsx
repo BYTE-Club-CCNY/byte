3f464b12bb02d068c6bb2bbe895f1021f68639b3
Evan <evanhaque1@gmail.com>
2024-05-27 20:58:30 -0400

    feat: Meed Cabinet members component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 import React from "react";
 import { HeroScroll } from "@/components/ui/HeroScroll";
 import AboutSection from "@/components/About";
+import Eboard from "@/components/Eboard";
 export default function Home() {
   return (
     <main>
       <HeroScroll />
       <AboutSection />
+      <Eboard />
     </main>
   );
 }
