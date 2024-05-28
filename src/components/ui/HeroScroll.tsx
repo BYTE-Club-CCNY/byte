89c639d2daf5220a84684dedc5c14c73a706568b
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-05-27 23:44:20 -0400

    chore:removed unncessary imports

--- src/components/ui/HeroScroll.tsx
+++ src/components/ui/HeroScroll.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { ContainerScroll } from "./container-scroll-animation";
 import Image from "next/image";
-import { relative } from "path";
 
 export function HeroScroll() {
   return (
