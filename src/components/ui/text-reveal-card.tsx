7881fff64b980c52dabae3c9b8dc55b92d4c009f
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-05-27 22:51:55 -0400

    feat:minor positioning fixes

---
+++ src/components/ui/text-reveal-card.tsx
@@ -0,0 +1,5 @@
+"use client";
+import React, { useEffect, useRef, useState, memo } from "react";
+import { motion } from "framer-motion";
+import { twMerge } from "tailwind-merge";
+import { cn } from "@/utils/cn";
