2e7571c0b83da77b5cce84d74030eef5e0a0182c
jaynopponep <noppone.p@gmail.com>
2024-05-29 21:14:47 -0400

    fixed navbar completely (todo: linking

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -53,7 +53,7 @@ export const FloatingNav = ({ navItems, className }: FloatingNavProps) => {
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] pr-3 pl-3 py-2 items-center justify-center space-x-3",
+          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] pr-2 pl-2 py-1 px-5 items-center justify-center space-x-0.2",
           className,
         )}
       >
@@ -62,19 +62,13 @@ export const FloatingNav = ({ navItems, className }: FloatingNavProps) => {
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 flex items-center justify-center py-2 px-4 bg-white rounded-full shadow-md",
+              "relative dark:text-neutral-50 flex items-center justify-center py-2 px-4 bg-white rounded-full",
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
             <span className="hidden sm:block text-sm">{navItem.name}</span>
           </Link>
         ))}
-        {/* <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
-          <Link href="/contact">
-            <span>Contact Us</span>
-          </Link>
-          <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
-        </button> */}
       </motion.div>
     </AnimatePresence>
   );
