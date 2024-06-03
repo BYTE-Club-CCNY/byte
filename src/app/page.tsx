4425a268b0c9f23754752b2a5424b112a328c888
jaynopponep <noppone.p@gmail.com>
2024-06-03 18:57:27 -0400

    new project carousel blueprint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,20 @@
 import React from "react";
 import { HeroScroll } from "@/components/ui/HeroScroll";
 import Eboard from "@/components/Eboard";
+import EmblaCarousel from "@/components/ui/carousel";
+import { EmblaOptionsType } from "embla-carousel";
+import "@/components/ui/css/embla.css";
+import "@/components/ui/css/sandbox.css";
+
+const OPTIONS: EmblaOptionsType = {};
+const SLIDE_COUNT = 10;
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 export default function Home() {
   return (
     <main className="light:bg-[#e5e7eb]">
       <HeroScroll />
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
       <Eboard />
     </main>
   );
---
+++ src/components/ui/css/README.md
@@ -0,0 +1,1 @@
+For just the project carousel component, will use vanilla CSS since some CSS values are custom to embla and cannot be applied to tailwind. Everything should still work well though.
