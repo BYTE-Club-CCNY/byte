f5cf9ce44354cce9e8e8cccac98e9db21026e440
jaynopponep <noppone.p@gmail.com>
2024-06-03 20:20:37 -0400

    fixed bug

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,21 +3,23 @@ import GoToProject from "@/components/GoToProject";
 const Projects = async () => {
   const projects = await fetch("http://18.117.235.144/projects");
   const projectsJson = await projects.json();
-  <>
-    <h1>Projects</h1>
+  return (
+    <>
+      <h1>Projects</h1>
 
-    {projectsJson.map(
-      (project: any) => (
-        console.log(project.name),
-        (
-          <>
-            <p>{project.name}</p>
-            <GoToProject name={project.name} />
-          </>
-        )
-      ),
-    )}
-  </>;
+      {projectsJson.map(
+        (project: any) => (
+          console.log(project.name),
+          (
+            <>
+              <p>{project.name}</p>
+              <GoToProject name={project.name} />
+            </>
+          )
+        ),
+      )}
+    </>
+  );
 };
 
 export default Projects;
