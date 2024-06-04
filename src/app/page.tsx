708540ae8c046c9d27e57c8026a61b2646d8a5b0
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-06-04 13:54:44 -0400

    wrapped api call in useeffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,35 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { HeroScroll } from "@/components/ui/HeroScroll";
 import Eboard from "@/components/Eboard";
 import EmblaCarousel from "@/app/projects/carousel";
 import { EmblaOptionsType } from "embla-carousel";
 import "@/components/ui/css/embla.css";
 import "@/components/ui/css/sandbox.css";
-
 const OPTIONS: EmblaOptionsType = {};
 
-export default async function Home() {
-  const projects = await fetch("http://18.117.235.144/projects");
-  const projectsJson = await projects.json();
+export default function Home() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await fetch("http://18.117.235.144/projects");
+        const data = await response.json();
+        setProjects(data);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
   return (
     <main className="light:bg-[#e5e7eb]">
       <HeroScroll />
       <EmblaCarousel
-        slides={projectsJson.map((project: any) => project.name)}
+        slides={projects.map((project: any) => project.name)}
         options={OPTIONS}
       />
       <Eboard />
