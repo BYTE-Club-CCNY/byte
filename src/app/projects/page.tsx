994f0d1ee7f130e84a9a2801bcb4225acd9727ea
jaynopponep <noppone.p@gmail.com>
2024-06-03 20:16:44 -0400

    edit

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,25 +3,21 @@ import GoToProject from "@/components/GoToProject";
 const Projects = async () => {
   const projects = await fetch("http://18.117.235.144/projects");
   const projectsJson = await projects.json();
-  return projectsJson;
+  <>
+    <h1>Projects</h1>
+
+    {projectsJson.map(
+      (project: any) => (
+        console.log(project.name),
+        (
+          <>
+            <p>{project.name}</p>
+            <GoToProject name={project.name} />
+          </>
+        )
+      ),
+    )}
+  </>;
 };
 
 export default Projects;
-
-// (
-//   <>
-//     <h1>Projects</h1>
-
-//     {projectsJson.map(
-//       (project: any) => (
-//         console.log(project.name),
-//         (
-//           <>
-//             <p>{project.name}</p>
-//             <GoToProject name={project.name} />
-//           </>
-//         )
-//       ),
-//     )}
-//   </>
-// );
