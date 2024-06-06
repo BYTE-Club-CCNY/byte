78a64563b7cff7754bd5fc75ba4a6cacd895e382
Fahad Faruqi <fahadfaruqi1@gmail.com>
2024-06-06 11:31:43 -0400

    added cloudfront domain to allowed images source

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
       "cdn.pixabay.com",
       "static.vecteezy.com",
       "codingwithkaushal.com",
-      "byte-media.s3.us-east-2.amazonaws.com",
+      "d1tzawfcgeew72.cloudfront.net",
     ],
   },
 };
