ec6f39f579449dd0080b87f63813188c63c260ad
jaynopponep <noppone.p@gmail.com>
2024-06-25 12:32:14 -0400

    fixed padding with title, left and right of popup

--- src/app/projects/popup.tsx
+++ src/app/projects/popup.tsx
@@ -2,14 +2,29 @@ import React, { useState, useEffect } from "react";
 import { IoLogoGithub } from "react-icons/io5";
 import { Divider } from "@nextui-org/divider";
 
+interface PopupGridProps {
+    name: string;
+}
+
+interface Project {
+    name: string;
+    ["long-desc"]: string;
+    cohort: string;
+    team: string[];
+    ["tech-stack"]: string[];
+    topic: string[];
+    link: string;
+    image?: string;
+}
+
 export function PopupGrid({ name }: PopupGridProps) {
-    const [project, setProject] = useState<any[]>([]);
+    const [project, setProject] = useState<Project[]>([]);
 
     useEffect(() => {
-        const getProjects = async (name: any) => {
+        const getProjects = async (name: string) => {
             try {
                 const response = await fetch(
-                    `https://test.byteccny.com/projects/get?name=${name}`,
+                    `https://test.byteccny.com/projects/get?name=${name}`
                 );
                 const data = await response.json();
                 setProject(data);
@@ -28,30 +43,32 @@ export function PopupGrid({ name }: PopupGridProps) {
                 {project.map((project, index) => (
                     <div
                         key={index}
-                        className="project-card flex flex-row justify-center items-center space-x-4 mb-8"
+                        className="pl-4 project-card flex flex-row justify-center items-center space-x-4 mb-8"
                     >
                         <div className="flex flex-col items-center justify-center flex-1">
-                            <div className="text-4xl font-sans font-semibold text-black">
+                            <div className="text-4xl font-sans font-semibold text-black mb-3">
                                 {project.name}
                             </div>
                             <div className="text-base text-black text-ellipsis whitespace-normal mb-4">
                                 {project["long-desc"]}
                             </div>
                             <Divider orientation='horizontal' className="my-1 bg-black"/>
-                            <div className="flex flex-row space-x-4 items-center">
-                                <div className="rounded-lg p-4 bg-white shadow">
-                                    <strong>Cohort:</strong> {project.cohort}
+                            <div className="flex flex-row w-full">
+                                <div className="flex-2 p-4 bg-white shadow rounded-lg">
+                                    <strong>Cohort:</strong>
+                                    <p>{project.cohort}</p>
                                 </div>
-                                <Divider orientation="vertical" className="mx-2 h-12" />
-                                <div className="rounded-lg p-4 bg-white shadow">
-                                    <strong>Team:</strong> {project.team.join(", ")}
+                                <div className="flex-2 p-4 bg-white shadow rounded-lg">
+                                    <strong>Team:</strong>
+                                    <p>{project.team.join(" ")}</p>
                                 </div>
-
-                                <div className="rounded-lg p-4 bg-white shadow">
-                                    <strong>Tech-Stack:</strong> {project["tech-stack"].join(", ")}
+                                <div className="flex-2 p-4 bg-white shadow rounded-lg">
+                                    <strong>Tech-Stack:</strong>
+                                    <p>{project["tech-stack"].join("  ")}</p>
                                 </div>
-                                <div className="rounded-lg p-4 bg-white shadow">
-                                    <strong>Topic:</strong> {project.topic.join(", ")}
+                                <div className="flex-2 p-4 bg-white shadow rounded-lg">
+                                    <strong>Topic:</strong>
+                                    <p>{project.topic.join(" ")}</p>
                                 </div>
                             </div>
                             <div className="mt-4">
@@ -63,7 +80,7 @@ export function PopupGrid({ name }: PopupGridProps) {
                                 </a>
                             </div>
                         </div>
-                        <div className="max-h-55 max-w-md object-cover rounded-md">
+                        <div className="pl-8 pr-4 max-h-55 max-w-md object-cover rounded-md">
                             {project.image && (
                                 <img
                                     className="rounded-md aspect-ratio"
