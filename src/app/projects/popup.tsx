93a1a75f697eaca69903e2f032206e6cd8c6fba0
jaynopponep <noppone.p@gmail.com>
2024-06-26 18:04:17 -0400

    finalizing design of project page

--- src/app/projects/popup.tsx
+++ src/app/projects/popup.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { IoLogoGithub } from "react-icons/io5";
 import { Divider } from "@nextui-org/divider";
+import { IoClose } from "react-icons/io5";
 
 interface PopupGridProps {
   name: string;
+  onClose: () => void; // Add this line
 }
 
 interface Project {
@@ -17,7 +19,8 @@ interface Project {
   image?: string;
 }
 
-export function PopupGrid({ name }: PopupGridProps) {
+export function PopupGrid({ name, onClose }: PopupGridProps) {
+  // Update this line
   const [project, setProject] = useState<Project[]>([]);
 
   useEffect(() => {
@@ -32,16 +35,18 @@ export function PopupGrid({ name }: PopupGridProps) {
         console.error("Error fetching projects:", error);
       }
     };
-    console.log(project)
+    console.log(project);
     if (name) {
       getProjects(name);
     }
   }, [name]);
 
-
   return (
     <>
-      <div className="mt-10 mb-12 overflow-hidden">
+      <div className="mt-1 mb-1 overflow-hidden relative">
+        <button onClick={onClose} className="absolute -top-2 -right-2 p-1">
+          <IoClose size={24} />
+        </button>
         {project.map((project, index) => (
           <div
             key={index}
@@ -56,32 +61,49 @@ export function PopupGrid({ name }: PopupGridProps) {
               </div>
               <Divider orientation="horizontal" className="my-1 bg-black" />
               <div className="flex flex-row w-full">
-              <div className="flex-2 flex flex-col p-4 items-center text-black bg-white shadow mx-2 rounded-lg space-y-2 w-1/4 h-36">
+                <div className="flex-2 flex flex-col p-4 items-center text-black bg-white shadow mx-2 rounded-lg space-y-2 w-1/4 h-36">
                   <strong>Cohort:</strong>
-                  <button className="text-black border-2 border-gray-500 p-1">{project.cohort}</button>
+                  <button className="text-black border-2 border-gray-500 p-1">
+                    {project.cohort}
+                  </button>
                 </div>
                 <div className="flex flex-col p-4 items-center text-black bg-white shadow mx-2 rounded-lg w-1/4 h-36">
                   <strong>Team:</strong>
                   <div className="flex flex-wrap items-center justify-center overflow-scroll">
-                  {project.team.map((member, index) => (
-                    <button className="text-black border-2 m-auto border-gray-500 mx-1 mb-2 p-1"key={index}>{member}</button>
-                  ))}
+                    {project.team.map((member, index) => (
+                      <button
+                        className="text-black border-2 m-auto border-gray-500 mx-1 mb-2 p-1"
+                        key={index}
+                      >
+                        {member}
+                      </button>
+                    ))}
                   </div>
                 </div>
                 <div className="flex flex-col p-4 items-center text-black bg-white shadow mx-2 rounded-lg w-1/4 h-36">
                   <strong>Tech-Stack:</strong>
                   <div className="flex flex-wrap items-center justify-center overflow-scroll">
-                  {project["tech-stack"].map((member, index) => (
-                    <button className="text-black border-2 border-gray-500 m-auto mb-2 p-1"key={index}>{member}</button>
-                  ))}
+                    {project["tech-stack"].map((member, index) => (
+                      <button
+                        className="text-black border-2 border-gray-500 m-auto mb-2 p-1"
+                        key={index}
+                      >
+                        {member}
+                      </button>
+                    ))}
                   </div>
                 </div>
                 <div className="flex-2 flex flex-col p-4 items-center text-black bg-white shadow mx-2 rounded-lg space-y-2 h-36 w-1/4">
                   <strong>Topic:</strong>
                   <div className="flex flex-wrap items-center justify-center overflow-scroll">
-                  {project.topic.map((member, index) => (
-                    <button className="text-black border-2 border-gray-500 m-auto mx-2 my-2 p-1"key={index}>{member}</button>
-                  ))}
+                    {project.topic.map((member, index) => (
+                      <button
+                        className="text-black border-2 border-gray-500 m-auto mx-2 my-2 p-1"
+                        key={index}
+                      >
+                        {member}
+                      </button>
+                    ))}
                   </div>
                 </div>
               </div>
@@ -98,7 +120,7 @@ export function PopupGrid({ name }: PopupGridProps) {
                 </a>
               </div>
             </div>
-            <div className="pl-8 pr-4 max-h-55 max-w-md object-cover rounded-md">
+            <div className="pl-8 pr-4 max-h-55 max-w-md object-cover rounded-md drop-shadow-[0_35px_15px_rgba(0.50,0.50,0.50,0.15)]">
               {project.image && (
                 <img
                   className="rounded-md aspect-ratio"
