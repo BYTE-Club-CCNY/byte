e4ebaea26157ab6bab0766849876666e622155c8
jaynopponep <noppone.p@gmail.com>
2024-07-20 17:01:14 -0400

    attempting to fix nonstatic position issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import "./globals.css";
+import { HeroScroll } from "@/components/ui/HeroScroll";
 
 const inter = Inter({ subsets: ["latin"] });
 
