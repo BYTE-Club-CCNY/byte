f93240ce6b543bbd0ad69547742d12f81573e531
joewadk <jkabir000@citymail.cuny.edu>
2024-08-25 14:49:38 -0400

    added higher z index to fix navbar issue. will look into fixing scroll bar

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -53,7 +53,7 @@ export const FloatingNav = ({ navItems, className }: FloatingNavProps) => {
           duration: 0.1,
         }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] pr-2 pl-2 py-1 px-5 items-center justify-center space-x-0.2",
+          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-black bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] pr-2 pl-2 py-1 px-5 items-center justify-center space-x-0.2 z-50",
           className,
         )}
       >
--- todo.md
+++ todo.md
@@ -1,2 +1,5 @@
 - [ ] Setup github action for Amplify 
 https://medium.com/@rkssh-daas/set-up-github-to-aws-amplify-integration-with-github-actions-for-ci-cd-8bf8b087324c
+- [ ] fix scroll bar on page so images match correctly with text
+- [x] fix navbar - the issue has been identified on floating-navbar.tsx
+- [ ] 
